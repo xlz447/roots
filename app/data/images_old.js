bd197fd6ec8225e15b1a825336496fe890c4a895
zfeng <zfeng@student.42.us.org>
2018-11-13 12:55:41 -0800

    modify the chart with new images.json

---
+++ app/data/images_old.js
@@ -0,0 +1,469 @@
+var json = {
+  "images": [
+    {
+      "image_id": 1,
+      "tags":
+      {
+        "brightness": 8,
+        "geometry": 3.5,
+        "temperature": 3,
+        "saturation": 5,
+        "density": 1,
+        "transparency": 6,
+        "roughness": 2
+      }
+    },
+    {
+      "image_id": 2,
+      "tags":
+      {
+        "brightness": 7,
+        "geometry": 6,
+        "temperature": 7,
+        "saturation": 9,
+        "density": 6,
+        "transparency": 3,
+        "roughness": 7
+      }
+    },
+    {
+      "image_id": 4,
+      "tags":
+      {
+        "brightness": 6.5,
+        "geometry": 3.5,
+        "temperature": 2,
+        "saturation": 3,
+        "density": 4.5,
+        "transparency": 1,
+        "roughness": 3
+      }
+    },
+    {
+      "image_id": 6,
+      "tags":
+      {
+        "brightness": 4.5,
+        "geometry": 3.8,
+        "temperature": 1,
+        "saturation": 1,
+        "density": 10,
+        "transparency": 2.7,
+        "roughness": 5.5
+      }
+    },
+    {"image_id": 7,
+      "tags":
+      {
+        "brightness": 2,
+        "geometry": 6.8,
+        "temperature": 4.5,
+        "saturation": 1,
+        "density": 7,
+        "transparency": 2.8,
+        "roughness": 4
+      }
+    },
+    {
+      "image_id": 8,
+      "tags":
+      {
+        "brightness": 1,
+        "geometry": 7,
+        "temperature": 6,
+        "saturation": 1,
+        "density": 6.7,
+        "transparency": 1,
+        "roughness": 1
+      }
+    },
+    {
+      "image_id": 9,
+      "tags":
+      {
+        "brightness": 5,
+        "geometry": 7,
+        "temperature": 7.8,
+        "saturation": 6.8,
+        "density": 7,
+        "transparency": 1,
+        "roughness": 5
+      }
+    },
+    {
+      "image_id": 10,
+      "tags":
+      {
+        "brightness": 2.8,
+        "geometry": 7.8,
+        "temperature": 4,
+        "saturation": 9,
+        "density": 8,
+        "transparency": 3,
+        "roughness": 1
+      }
+    },
+    {"image_id": 11,
+      "tags":
+      {
+        "brightness": 4.5,
+        "geometry": 3,
+        "temperature": 2.5,
+        "saturation": 1,
+        "density": 7,
+        "transparency": 2,
+        "roughness": 2
+      }
+    },
+    {
+      "image_id": 12,
+      "tags":
+      {
+        "brightness": 4,
+        "geometry": 5,
+        "temperature": 4,
+        "saturation": 1,
+        "density": 6,
+        "transparency": 5,
+        "roughness": 6
+      }
+    },
+    {
+      "image_id": 13,
+      "tags":
+      {
+        "brightness": 6,
+        "geometry": 7.5,
+        "temperature": 9,
+        "saturation": 10,
+        "density": 2,
+        "transparency": 1,
+        "roughness": 8
+      }
+    },
+    {
+      "image_id": 15,
+      "tags":
+      {
+        "brightness": 8,
+        "geometry": 9.5,
+        "temperature": 4.5,
+        "saturation": 4.5,
+        "density": 1,
+        "transparency": 2,
+        "roughness": 1
+      }
+    },
+    {
+      "image_id": 16,
+      "tags":
+      {
+        "brightness": 1,
+        "geometry": 7,
+        "temperature": 6.5,
+        "saturation": 1,
+        "density": 8,
+        "transparency": 2,
+        "roughness": 8
+      }
+    },
+    {
+      "image_id": 17,
+      "tags": {
+        "brightness": 2.3,
+        "geometry": 7.3,
+        "temperature": 4.5,
+        "saturation": 1,
+        "density": 2.5,
+        "transparency": 7,
+        "roughness": 3.2
+        }
+      },
+    {
+      "image_id": 18,
+      "tags":
+      {
+        "brightness": 7.3,
+        "geometry": 2.8,
+        "temperature": 6.5,
+        "saturation": 7,
+        "density": 6.2,
+        "transparency": 2,
+        "roughness": 6.2
+      }
+    },
+    {
+      "image_id": 19,
+      "tags":
+      {
+        "brightness": 2.8,
+        "geometry": 1,
+        "temperature": 3,
+        "saturation": 9.5,
+        "density": 2.5,
+        "transparency": 7,
+        "roughness": 2
+      }
+    },
+    {
+      "image_id": 20,
+      "tags":
+      {
+        "brightness": 7,
+        "geometry": 6,
+        "temperature": 7,
+        "saturation": 9,
+        "density": 6,
+        "transparency": 3,
+        "roughness": 7
+      }
+    },
+    {
+      "image_id": 21,
+      "tags":
+      {
+        "brightness": 1.8,
+        "geometry": 7,
+        "temperature": 7,
+        "saturation": 1,
+        "density": 7.5,
+        "transparency": 1.5,
+        "roughness": 5
+      }
+    },
+    {
+      "image_id": 23,
+      "tags":
+      {
+        "brightness": 4,
+        "geometry": 1,
+        "temperature": 2,
+        "saturation": 1,
+        "density": 8,
+        "transparency": 3,
+        "roughness": 5.5
+      }
+    },
+    {
+      "image_id": 24,
+      "tags":
+      {
+        "brightness": 4,
+        "geometry": 2.8,
+        "temperature": 6,
+        "saturation": 6,
+        "density": 3,
+        "transparency": 1,
+        "roughness": 4
+      }
+    },
+    {
+      "image_id": 28,
+      "tags":
+      {
+        "brightness": 4.5,
+        "geometry": 7.5,
+        "temperature": 7.5,
+        "saturation": 3.5,
+        "density": 5.5,
+        "transparency": 1.5,
+        "roughness": 5
+      }
+    },
+    {
+      "image_id": 29,
+      "tags":
+      {
+        "brightness": 5,
+        "geometry": 8.5,
+        "temperature": 8,
+        "saturation": 9,
+        "density": 8,
+        "transparency": 1,
+        "roughness": 3.5
+      }
+    },
+    {
+      "image_id": 32,
+      "tags":
+      {
+        "brightness": 7.5,
+        "geometry": 6,
+        "temperature": 9.5,
+        "saturation": 10,
+        "density": 6.5,
+        "transparency": 3,
+        "roughness": 5
+      }
+    },
+    {
+      "image_id": 38,
+      "tags":
+      {
+        "brightness": 8,
+        "geometry": 3,
+        "temperature": 1,
+        "saturation": 3,
+        "density": 2,
+        "transparency": 8,
+        "roughness": 3.5
+      }
+    },
+    {
+      "image_id": 40,
+      "tags":
+      {
+        "brightness": 8,
+        "geometry": 3.8,
+        "temperature": 6,
+        "saturation": 6,
+        "density": 7.5,
+        "transparency": 3,
+        "roughness": 7.5
+      }
+    },
+    {
+      "image_id": 44,
+      "tags":
+      {
+        "brightness": 4,
+        "geometry": 5,
+        "temperature": 5,
+        "saturation": 7,
+        "density": 3,
+        "transparency": 7,
+        "roughness": 7.5
+      }
+    },
+    {
+      "image_id": 45,
+      "tags":
+      {
+        "brightness": 7.5,
+        "geometry": 7,
+        "temperature": 7,
+        "saturation": 8.5,
+        "density": 2.5,
+        "transparency": 7.5,
+        "roughness": 7.3
+      }
+    },
+    {
+      "image_id": 108,
+      "tags":
+      {
+        "brightness": 3.07,
+        "geometry": 6.93,
+        "temperature": 6.43,
+        "saturation": 7,
+        "density": 7,
+        "transparency": 3.71,
+        "roughness": 6
+      }
+    },
+    {
+      "image_id": 123,
+      "tags":
+      {
+        "brightness": 7.54,
+        "geometry": 5.85,
+        "temperature": 3.77,
+        "saturation": 6.85,
+        "density": 3.92,
+        "transparency": 7.85,
+        "roughness": 4.08
+      }
+    },
+    {
+      "image_id": 199,
+      "tags":
+      {
+        "brightness": 3.93,
+        "geometry": 6.21,
+        "temperature": 7,
+        "saturation": 5.57,
+        "density": 6.14,
+        "transparency": 2.5,
+        "roughness": 4.86
+      }
+    },
+    {
+      "image_id": 200,
+      "tags":
+      {
+        "brightness": 6.31,
+        "geometry": 3.85,
+        "temperature": 1.69,
+        "saturation": 2.31,
+        "density": 5.08,
+        "transparency": 4.77,
+        "roughness": 5.85
+      }
+    },
+    {
+      "image_id": 201,
+      "tags":
+      {
+        "brightness": 8.69,
+        "geometry": 3.92,
+        "temperature": 3,
+        "saturation": 4.31,
+        "density": 4.77,
+        "transparency": 4.69,
+        "roughness": 5
+      }
+    },
+    {
+      "image_id": 202,
+      "tags":
+      {
+        "brightness": 3.31,
+        "geometry": 4.46,
+        "temperature": 6.15,
+        "saturation": 5.69,
+        "density": 6.08,
+        "transparency": 4.31,
+        "roughness": 5.54
+      }
+    },
+    {
+      "image_id": 203,
+      "tags":
+      {
+        "brightness": 8.08,
+        "geometry": 4.62,
+        "temperature": 7.08,
+        "saturation": 7.08,
+        "density": 5,
+        "transparency": 5.62,
+        "roughness": 6.85
+      }
+    },
+    {
+      "image_id": 204,
+      "tags":
+      {
+        "brightness": 1.91,
+        "geometry": 6.23,
+        "temperature": 4.92,
+        "saturation": 6.08,
+        "density": 7.08,
+        "transparency": 1.92,
+        "roughness": 3.62
+      }
+    },
+    {
+      "image_id": 205,
+      "tags":
+      {
+        "brightness": 8.69,
+        "geometry": 4.23,
+        "temperature": 6.54,
+        "saturation": 6.23,
+        "density": 4.62,
+        "transparency": 5.31,
+        "roughness": 7.08
+      }
+    }
+  ]
+}
